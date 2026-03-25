4cd81761685c7afd4e1c4891b33ec891822bebbb
2NiRoF <132067328+2NiRoF@users.noreply.github.com>
2026-03-26 00:42:18 +0900

    [Silver V] Title: 방 번호, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/1475. 방 번호/방 번호.py
@@ -0,0 +1,15 @@
+n = input()
+cnt = [0,0,0,0,0,0,0,0,0,0]
+
+for i in range(10):
+    for j in range(len(n)):
+        if int(n[j]) == i:
+            cnt[i] += 1
+
+sixnine = (cnt[6]+cnt[9])//2 + 1 if (cnt[6]+cnt[9])%2 == 1 else (cnt[6]+cnt[9])//2
+cnt[6] = sixnine
+cnt[9] = 0  
+maxcnt = max(cnt)
+maxnum = cnt.index(maxcnt)
+
+print(maxcnt)
